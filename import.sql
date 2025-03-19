b18a96142aa201cb89951dd12743bb952a2dc7e6
andrei.jara <andrei.jara@azi.com.br>
2025-03-18 21:11:23 -0400

    Revert "Criacao do JPA com H2DataBase"
    This reverts commit 91a54915f6ef21f45cc3ce8cd58e8df03beac74d.

--- import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO categoria (descricao, data_de_inclusao, data_de_alteracao) VALUES ('Bebidas', PARSEDATETIME('2024-09-02-00.00.00','yyyy-MM-dd-HH.mm.ss'), NULL);
-INSERT INTO categoria (descricao, data_de_inclusao, data_de_alteracao) VALUES ('Carnes', PARSEDATETIME('2024-09-02-00.00.00','yyyy-MM-dd-HH.mm.ss'), NULL);
-INSERT INTO categoria (descricao, data_de_inclusao, data_de_alteracao) VALUES ('Cereais', PARSEDATETIME('2024-09-02-00.00.00','yyyy-MM-dd-HH.mm.ss'), NULL);
-INSERT INTO categoria (descricao, data_de_inclusao, data_de_alteracao) VALUES ('Hortifruti', PARSEDATETIME('2024-09-02-00.00.00','yyyy-MM-dd-HH.mm.ss'), NULL);
-INSERT INTO categoria (descricao, data_de_inclusao, data_de_alteracao) VALUES ('Padaria', PARSEDATETIME('2024-09-02-00.00.00','yyyy-MM-dd-HH.mm.ss'), NULL);
